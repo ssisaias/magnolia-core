3f848dd98062d0850bfdbfc2d014dcdf7ea3e393
Isaias S. Silva <luiz.isaias95@gmail.com>
2018-04-08 14:20:27 -0300

    Configurable addresses to check

--- app/controllers/statusCheck.js
+++ app/controllers/statusCheck.js
@@ -1,11 +1,13 @@
 const StatusCheck = require('../models/statusCheck');
 var request = require('request');
+
+var URLS_TO_VERIFY = process.env.URLS_TO_CHECK;
 /**
  * GET /m/:name
  * Send Message Page for public user
  */
 exports.getStatuses = (req, res, next) => {
-    StatusCheck.find({} , (err, statuses) => {
+    StatusCheck.find({}, (err, statuses) => {
         if (err) {
             return next(err);
         }
@@ -14,12 +16,18 @@ exports.getStatuses = (req, res, next) => {
             return res.status(204).send('No content');
         }
         return res.status(200).send(statuses);
-    }).sort({ date:-1 });
+    }).sort({
+        date: -1
+    });
 };
 
 /* Test create*/
 exports.testAddNew = (req, res, next) => {
-    var st = new StatusCheck({ code: 'ahaha', status:'test', date: Date() });
+    var st = new StatusCheck({
+        code: 'ahaha',
+        status: 'test',
+        date: Date()
+    });
     st.save((err) => {
         if (err) {
             return next(err);
@@ -30,46 +38,59 @@ exports.testAddNew = (req, res, next) => {
 
 /* Make a requisition to sistemas.quixada.ufc*/
 exports.makereq = (req, res, next) => {
-    console.log('Trying To HIT https://httpstat.us/200');
-    request('https://hdasdsdfarfqrft.us/400',{ timeout:'30000' }, function (error, response) {
-        console.log('error:', error); // Print the error if one occurred
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        if (error){
-            var st = new StatusCheck({ code: error.Error, status:'OFFLINE', date: Date() });
-            st.save((err) => {
-                if (err) {
-                    return next(err);
+    URLS_TO_VERIFY = JSON.parse(URLS_TO_VERIFY);
+    console.log(URLS_TO_VERIFY);
+    URLS_TO_VERIFY.forEach(targetUrl => {
+        console.log('Trying To HIT: ' + targetUrl);
+        request(targetUrl, {
+            timeout: '30000'
+        }, function (error, response) {
+            console.log('error:', error); // Print the error if one occurred
+            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+            if (error) {
+                var st = new StatusCheck({
+                    code: error.Error,
+                    status: 'OFFLINE',
+                    date: Date(),
+                    address: targetUrl,
+                });
+                st.save((err) => {
+                    if (err) {
+                        return next(err);
+                    }
+                    // return res.status(200).send('OK');
+                });
+            }
+            if (response && response.statusCode) {
+                if (response.statusCode.toString().startsWith('2')) {
+                    var st = new StatusCheck({
+                        code: response.statusCode,
+                        status: 'ONLINE',
+                        date: Date(),
+                        address: targetUrl,
+                    });
+                    st.save((err) => {
+                        if (err) {
+                            return next(err);
+                        }
+                        // return res.status(200).send('OK');
+                    });
+                } else {
+                    var st = new StatusCheck({
+                        code: response.statusCode,
+                        status: 'PROBLEM',
+                        date: Date(),
+                        address: targetUrl,
+                    });
+                    st.save((err) => {
+                        if (err) {
+                            return next(err);
+                        }
+                        // return res.status(200).send('OK');
+                    });
                 }
-                // return res.status(200).send('OK');
-            });
-        }
-        if (response && response.statusCode){
-            if (response.statusCode.toString().startsWith('2')){
-                var st = new StatusCheck({ code: response.statusCode, status:'ONLINE', date: Date() });
-                st.save((err) => {
-                    if (err) {
-                        return next(err);
-                    }
-                    // return res.status(200).send('OK');
-                });
             }
-            else {
-                var st = new StatusCheck({ code: response.statusCode, status:'PROBLEM', date: Date() });
-                st.save((err) => {
-                    if (err) {
-                        return next(err);
-                    }
-                    // return res.status(200).send('OK');
-                });
-            }
-        }
+        });
     });
-/* 
-    var st = new StatusCheck({ code: 'ahaha', status:'test', date: Date() });
-    st.save((err) => {
-        if (err) {
-            return next(err);
-        }
-        return res.status(200).send('OK');
-    }); */
+    
 };
